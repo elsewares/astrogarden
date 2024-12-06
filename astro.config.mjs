5cf1cd243266c6d1730924a9834e820e3cc1951f
MaggieAppleton <5599295+MaggieAppleton@users.noreply.github.com>
2024-12-06 11:58:34 +0000

    added base layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://www.maggieappleton.com",
     image: {
         domains: ["res.cloudinary.com"],
     },
