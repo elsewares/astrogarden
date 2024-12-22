9a083ca3a1e4076c7b5071cbd0f40c9cdbde699e
MaggieAppleton <5599295+MaggieAppleton@users.noreply.github.com>
2024-12-22 20:57:20 +0000

    made backlinks work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
+import { remarkWikiLink } from "./src/plugins/remark-wiki-link";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.maggieappleton.com",
-    image: {
-        domains: ["res.cloudinary.com"],
-    },
-    integrations: [mdx(), react(), icon()],
+	site: "https://www.maggieappleton.com",
+	image: {
+		domains: ["res.cloudinary.com"],
+	},
+	integrations: [
+		mdx({
+			remarkPlugins: [remarkWikiLink],
+		}),
+		react(),
+		icon(),
+	],
 });
