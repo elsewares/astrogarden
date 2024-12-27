e53f50ae1c9c44c851e286b658bfdb5fc5e47c3c
MaggieAppleton <5599295+MaggieAppleton@users.noreply.github.com>
2024-12-27 16:41:50 +0000

    analytics setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,25 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 import { remarkWikiLink } from "./src/plugins/remark-wiki-link";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.maggieappleton.com",
-	image: {
-		domains: ["res.cloudinary.com"],
-	},
-	integrations: [
-		mdx({
-			remarkPlugins: [remarkWikiLink],
-		}),
-		react(),
-		icon(),
-	],
+  site: "https://www.maggieappleton.com",
+  image: {
+    domains: ["res.cloudinary.com"],
+  },
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkWikiLink],
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    react(),
+    icon(),
+  ],
 });
